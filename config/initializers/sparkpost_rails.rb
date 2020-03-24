1e6d8894b0795e9cc6c155cc4f253087499b9e0f
Erik John Moreno <erikm@sourcepad.com>
2020-03-24 03:16:02 -0400

    comment temporarily the initializers/sparkpost_rails.rb

--- config/initializers/sparkpost_rails.rb
+++ config/initializers/sparkpost_rails.rb
@@ -1,5 +1,5 @@
-SparkPostRails.configure do |c|
-  c.api_key = Rails.configuration.settings['sparkpost_api_key']
-  c.inline_css = true
-  c.html_content_only = true
-end
+# SparkPostRails.configure do |c|
+#   c.api_key = Rails.configuration.settings['sparkpost_api_key']
+#   c.inline_css = true
+#   c.html_content_only = true
+# end
